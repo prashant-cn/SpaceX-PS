3277ffc729c624bf95d22360ee77e1406f20cf9a
Prashant Kumar <prashant_hem@yahoo.co.in>
2020-10-11 13:18:16 +0530

    PORT Issue

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const commonFunctions = require('./utils/commonFunctions')
 
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const basePath = path.join(__dirname, '../public')
 app.use(express.static(basePath))
